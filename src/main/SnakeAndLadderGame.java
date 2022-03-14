82ab6e9ab226c6d2dc9424b1e225631f61a75115
Sunny996 <satlaakash@gmail.com>
2022-03-14 19:41:19 +0530

    ADDED: A player instance variable with position at 0

---
+++ src/main/SnakeAndLadderGame.java
@@ -0,0 +1,10 @@
+package main;
+
+public class SnakeAndLadderGame {
+   int playerPosition=0;
+
+
+
+
+}
+
---
+++ src/main/SnakeAndLadderTest.java
@@ -0,0 +1,11 @@
+package main;
+
+public class SnakeAndLadderTest {
+    public static void main(String[] args) {
+        SnakeAndLadderGame game = new SnakeAndLadderGame();
+//        System.out.println(game.diceRoll());
+//        game.option1();
+        System.out.println("Player Position at "+game.playerPosition);
+
+    }
+}
