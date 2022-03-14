53923f4d6147dd2985d3291190ed78a1c6cdd794
Sunny996 <satlaakash@gmail.com>
2022-03-14 15:46:40 +0530

    ADDED: A method for the player to reach winning position

--- src/main/SnakeAndLadderGame.java
+++ src/main/SnakeAndLadderGame.java
@@ -1,21 +1,29 @@
 package main;
 
 public class SnakeAndLadderGame {
-    int playerPosition = 0;
+
     int LADDER = 1;
     int SNAKE = 2;
-
-    public int diceRoll() {
-        return (int) (Math.random() * 10 % 6 + 1);
-    }
+    int MAX_BOARD_VALUE = 100;
+    int MIN_BOARD_VALUE = 0;
 
     public int option() {
-        int opt = (int) (Math.random() * 10 % 3);
-        int diceVal = ((int) (Math.random() * 10 % 6 + 1));
-        if (opt == LADDER)
-            playerPosition += diceVal;
-        else if (opt == SNAKE)
-            playerPosition -= diceVal;
+        int playerPosition = 0;
+        while (playerPosition < MAX_BOARD_VALUE) {
+            int opt = (int) (Math.random() * 10 % 3);
+            int diceVal = ((int) (Math.random() * 10 % 6 + 1));
+            switch (opt) {
+                case 1:
+                    playerPosition += diceVal;
+                    break;
+                case 2:
+                    if ((playerPosition - diceVal) < MIN_BOARD_VALUE)
+                        playerPosition = MIN_BOARD_VALUE;
+                    else
+                        playerPosition -= diceVal;
+                    break;
+            }
+        }
         return playerPosition;
     }
 }
--- src/main/SnakeAndLadderTest.java
+++ src/main/SnakeAndLadderTest.java
@@ -3,6 +3,6 @@ package main;
 public class SnakeAndLadderTest {
     public static void main(String[] args) {
         SnakeAndLadderGame game = new SnakeAndLadderGame();
-        System.out.println(game.option());
+        System.out.println("Player Position is at "+game.option());
     }
 }
