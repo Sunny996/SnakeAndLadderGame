169d0dc188926bbf86121c50582f02a24bf028ae
Sunny996 <satlaakash@gmail.com>
2022-03-14 20:20:54 +0530

    ADDED: A method to get player options

--- src/main/SnakeAndLadderGame.java
+++ src/main/SnakeAndLadderGame.java
@@ -2,9 +2,21 @@ package main;
 
 public class SnakeAndLadderGame {
     int playerPosition = 0;
+    int LADDER = 1;
+    int SNAKE = 2;
 
     public int diceRoll() {
         return (int) (Math.random() * 10 % 6 + 1);
+    }
+
+    public int option() {
+        int opt = (int) (Math.random() * 10 % 3);
+        int diceVal = ((int) (Math.random() * 10 % 6 + 1));
+        if (opt == LADDER)
+            playerPosition += diceVal;
+        else if (opt == SNAKE)
+            playerPosition -= diceVal;
+        return playerPosition;
     }
 }
 
--- src/main/SnakeAndLadderTest.java
+++ src/main/SnakeAndLadderTest.java
@@ -3,6 +3,6 @@ package main;
 public class SnakeAndLadderTest {
     public static void main(String[] args) {
         SnakeAndLadderGame game = new SnakeAndLadderGame();
-        System.out.println(game.diceRoll());
+        System.out.println(game.option());
     }
 }
