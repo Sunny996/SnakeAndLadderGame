e3b323c39952e5020b85e3e19dcca7120672d8b8
Sunny996 <satlaakash@gmail.com>
2022-03-14 20:06:30 +0530

    ADDED: A method to roll die and get a number

--- src/main/SnakeAndLadderGame.java
+++ src/main/SnakeAndLadderGame.java
@@ -1,10 +1,10 @@
 package main;
 
 public class SnakeAndLadderGame {
-   int playerPosition=0;
+    int playerPosition = 0;
 
-
-
-
+    public int diceRoll() {
+        return (int) (Math.random() * 10 % 6 + 1);
+    }
 }
 
--- src/main/SnakeAndLadderTest.java
+++ src/main/SnakeAndLadderTest.java
@@ -3,9 +3,6 @@ package main;
 public class SnakeAndLadderTest {
     public static void main(String[] args) {
         SnakeAndLadderGame game = new SnakeAndLadderGame();
-//        System.out.println(game.diceRoll());
-//        game.option1();
-        System.out.println("Player Position at "+game.playerPosition);
-
+        System.out.println(game.diceRoll());
     }
 }
