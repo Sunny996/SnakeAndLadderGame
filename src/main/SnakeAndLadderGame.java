f9595a493c451d1faa42d6742094fa3e36116dfb
Akash Satla <65150847+Sunny996@users.noreply.github.com>
2022-03-16 08:31:55 +0530

    Update SnakeAndLadderGame.java

--- src/main/SnakeAndLadderGame.java
+++ src/main/SnakeAndLadderGame.java
@@ -4,34 +4,32 @@ public class SnakeAndLadderGame {
 
     public final int LADDER = 1;
     public final int SNAKE = 2;
-    public final int MAX_BOARD_VALUE = 100;
-    public final int MIN_BOARD_VALUE = 0;
+    public final int WINNING_POSITION = 100;
+    public final int START_POSITION = 0;
+
 
     public int option() {
-        int playerPosition = 0;
         int noOfDieRolls = 0;
-        while (playerPosition < MAX_BOARD_VALUE) {
+        int playerPosition = 0;
+        while (playerPosition < WINNING_POSITION) {
             int opt = (int) (Math.random() * 10 % 3);
             int diceVal = ((int) (Math.random() * 10 % 6 + 1));
             switch (opt) {
                 case LADDER:
-                    if ((playerPosition + diceVal) > MAX_BOARD_VALUE) {
-                    } else
-                        playerPosition += diceVal;
+                    playerPosition += diceVal;
+                    if (playerPosition > WINNING_POSITION)
+                        playerPosition -= diceVal;
                     break;
                 case SNAKE:
-                    if ((playerPosition - diceVal) < MIN_BOARD_VALUE)
-                        playerPosition = MIN_BOARD_VALUE;
-                    else
-                        playerPosition -= diceVal;
+                    playerPosition -= diceVal;
+                    if (playerPosition < START_POSITION)
+                        playerPosition = START_POSITION;
                     break;
             }
             noOfDieRolls++;
             System.out.println("Player Position :" + playerPosition);
-
         }
-        System.out.println("No of die rolls to win_" + noOfDieRolls);
+        System.out.println("No of die rolls to win " + noOfDieRolls);
         return playerPosition;
     }
 }
-
